ae5fb8b1642c14a58acc490249e0c41402641839
HEDO <hedo@oocl.com>
2024-12-02 15:12:42 +0800

    test: add should_return_the_given_todos_when_getAllTodoList test case

--- src/main/java/com/todo/entity/Todo.java
+++ src/main/java/com/todo/entity/Todo.java
@@ -40,4 +40,13 @@ public class Todo {
     public void setDone(Boolean done) {
         this.done = done;
     }
+
+    public Todo() {
+    }
+
+    public Todo(Integer id, String text, Boolean done) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
 }
--- src/test/java/com/todo/TodoControllerTest.java
+++ src/test/java/com/todo/TodoControllerTest.java
@@ -1,11 +1,23 @@
 package com.todo;
 
+import com.todo.entity.Todo;
 import com.todo.repository.TodoRepository;
 import com.todo.service.TodoService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 public class TodoControllerTest {
@@ -13,5 +25,110 @@ public class TodoControllerTest {
     TodoService todoService;
 
     @Mock
-    TodoRepository todoRepository;
+    TodoRepository mockedTodoRepository;
+
+    @Test
+    void should_return_the_given_todos_when_getAllTodoList() {
+        //given
+        when(mockedTodoRepository.findAll()).thenReturn(List.of(new Todo(1, "111", false)));
+
+        //when
+        List<Todo> allEmployees = todoService.getTodoList();
+
+        //then
+        assertEquals(1, allEmployees.size());
+        assertEquals("111", allEmployees.get(0).getText());
+        assertEquals(false, allEmployees.get(0).getDone());
+    }
+
+//    @Test
+//    void should_return_the_given_todos_when_getById() {
+//        //given
+//        when(mockedTodoRepository.findById(1)).thenReturn(Optional.of(new Todo(1, "111", false)));
+//
+//        //when
+//        Todo todo = todoService.getTodoById(1);
+//
+//        //then
+//        assertEquals("111", todo.getText());
+//        assertEquals(false, todo.getDone());
+//    }
+
+//    @Test
+//    void should_return_the_given_employees_when_findById() {
+//        //given
+//        Employee lucy = new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0);
+//        when(mockedEmployeeRepository.findById(1)).thenReturn(Optional.of(lucy));
+//
+//        //when
+//        Employee employee = employeeService.findById(1);
+//
+//        //then
+//        assertEquals("Lucy", employee.getName());
+//    }
+//
+//    @Test
+//    void should_return_the_created_employee_when_create_given_a_employee() {
+//        //given
+//        Employee lucy = new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0);
+//        when(employeeService.create(lucy)).thenReturn(lucy);
+//
+//        //when
+//        Employee createdEmployee = employeeService.create(lucy);
+//
+//        //then
+//        assertEquals("Lucy", createdEmployee.getName());
+//    }
+//
+//    @Test
+//    void should_throw_EmployeeAgeNotValidException_when_create_given_a_employee_with_age_17() {
+//        //given
+//        Employee kitty = new Employee(1, "Kitty", 6, Gender.FEMALE, 8000.0);
+//        //when
+//        //then
+//        assertThrows(EmployeeAgeNotValidException.class, () -> employeeService.create(kitty));
+//        verify(mockedEmployeeRepository, never()).save(any());
+//    }
+//
+//    @Test
+//    void should_throw_EmployeeAgeNotValidException_when_create_given_a_employee_with_age_66() {
+//        //given
+//        Employee kitty = new Employee(1, "Kitty", 66, Gender.FEMALE, 8000.0);
+//        //when
+//        //then
+//        assertThrows(EmployeeAgeNotValidException.class, () -> employeeService.create(kitty));
+//        verify(mockedEmployeeRepository, never()).save(any());
+//    }
+//
+//    @Test
+//    void should_created_employee_active_when_create_employee() {
+//        //given
+//        Employee lucy = new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0);
+//        //when
+//        employeeService.create(lucy);
+//        /* then */
+//        verify(mockedEmployeeRepository).save(argThat(Employee::getActive));
+//    }
+//
+//    @Test
+//    void should_throw_EmployeeAgeSalaryNotMatchedException_when_save_given_a_employee_with_age_over_30_and_salary_below_20K() {
+//        //given
+//        Employee bob = new Employee(1, "Bob", 31, Gender.FEMALE, 8000.0);
+//        //when
+//        //then
+//        assertThrows(EmployeeAgeSalaryNotMatchedException.class, () -> employeeService.create(bob));
+//        verify(mockedEmployeeRepository, never()).save(any());
+//    }
+//
+//    @Test
+//    void should_throw_EmployeeInactiveException_when_update_inactive_employee() {
+//        //given
+//        Employee inactiveEmployee = new Employee(1, "Bob", 31, Gender.FEMALE, 8000.0);
+//        inactiveEmployee.setActive(false);
+//        when(mockedEmployeeRepository.findById(1)).thenReturn(Optional.of(inactiveEmployee));
+//        //when
+//        //then
+//        assertThrows(EmployeeInactiveException.class, () -> employeeService.update(1, inactiveEmployee));
+//        verify(mockedEmployeeRepository, never()).save(any());
+//    }
 }
