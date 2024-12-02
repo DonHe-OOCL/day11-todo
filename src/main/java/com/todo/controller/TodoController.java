06118746a28f9203844b26e46963dda6a6ef5762
HEDO <hedo@oocl.com>
2024-12-02 15:02:54 +0800

    feat: add ResponseEntity

--- src/main/java/com/todo/controller/TodoController.java
+++ src/main/java/com/todo/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.todo.controller;
 
+import com.todo.common.ResponseEntity;
 import com.todo.entity.Todo;
 import com.todo.service.TodoService;
 import org.springframework.web.bind.annotation.*;
@@ -17,28 +18,28 @@ public class TodoController {
     }
 
     @GetMapping
-    public List<Todo> getTodos() {
-        return todoService.getTodoList();
+    public ResponseEntity<List<Todo>> getTodos() {
+        return ResponseEntity.success(todoService.getTodoList());
     }
 
     @GetMapping("/{id}")
-    public Todo getTodoById(@PathVariable Integer id) {
-        return todoService.getTodoById(id);
+    public ResponseEntity<Todo> getTodoById(@PathVariable Integer id) {
+        return ResponseEntity.success(todoService.getTodoById(id));
     }
 
     @PostMapping
-    public Todo addTodo(@RequestBody Todo todo) {
-        return todoService.addTodoItem(todo);
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
+        return ResponseEntity.success(todoService.addTodoItem(todo));
     }
 
     @PutMapping
-    public Todo updateTodoItem(@RequestBody Todo todo) {
-        return todoService.updateTodoItem(todo);
+    public ResponseEntity<Todo> updateTodoItem(@RequestBody Todo todo) {
+        return ResponseEntity.success(todoService.updateTodoItem(todo));
     }
 
     @DeleteMapping("/{id}")
-    public Integer deleteTodoById(@PathVariable Integer id) {
+    public ResponseEntity<Integer> deleteTodoById(@PathVariable Integer id) {
         todoService.deleteTodoItem(id);
-        return id;
+        return ResponseEntity.success(id);
     }
 }
